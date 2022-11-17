de801b151f229875b342c689d58f782bd6f1ffe0
Cacho Medina <zanlorenzocacho10@gmail.com>
2022-11-17 01:16:14 -0300

    añadiendo hash table

--- data-structure-II.js
+++ data-structure-II.js
@@ -1,6 +1,6 @@
 //LISTAS ENLAZADAS
 
-//LISTAS SIMPLES, DOBLES Y MULTIPLES
+//LISTAS SIMPLES, DOBLES Y CIRCULARES
 
 //creacion de lista y nodo
 function Node(data) {
@@ -13,9 +13,9 @@ function List() {
     this.head = null;
 }
 
-//implementacion de la funcion insertar nodo al final
+//implementacion de funciones a la lista
 
-List.prototype.add = function (data) {
+List.prototype.insertNode = function (data) {
     var node = new Node(data);
     var current = this.head; //referencia al primer nodo de la lista
     //si esta vacia
@@ -73,10 +73,80 @@ List.prototype.deleteNode = function (value) {
 };
 
 const list = new List();
-list.add(22);
-list.add(2);
-list.add(10);
+list.insertNode(22);
+list.insertNode(2);
+list.insertNode(10);
 list.print();
 console.log(list.findNode(2));
 list.deleteNode(22);
 list.print();
+
+/* ---------------------------------------------------------------------- */
+
+//HASH TABLE
+
+//USANDO MAP
+
+const hashTable = new Map();
+
+hashTable.set("name", "Cacho");
+hashTable.set("surname", "Medina");
+hashTable.set("edad", "22");
+
+console.log(hashTable.get("name"));
+console.log(hashTable.get("surname"));
+console.log(hashTable.size);
+
+for (const [key, value] of hashTable) {
+    console.log(`${key} = ${value}\n`);
+}
+
+//USANDO CLASES
+
+function tablaHash() {
+    this.tabla = [];
+    this.tamaño = 0;
+}
+
+tablaHash.prototype.set = function (key, value) {
+    const indice = this._hash(key);
+    this.tabla[indice] = [key, value];
+    this.tamaño++;
+};
+
+tablaHash.prototype.get = function (key) {
+    const indice = this._hash(key);
+    return this.tabla[indice];
+};
+
+tablaHash.prototype.remove = function (key) {
+    const indice = this._hash(key);
+    if (this.tabla[indice] && this.tabla[indice].length > 0) {
+        this.tabla[indice] = undefined;
+        this.tamaño--;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//el metodo _hash() es un metodo privado
+tablaHash.prototype._hash = function (key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+        hash += key.charCodeAt(i);
+    }
+    return hash % this.tabla.length;
+};
+
+const ht = new tablaHash();
+ht.set("name", "Cacho");
+ht.set("surname", "Medina");
+ht.set("age", 2);
+console.log(ht.get("name"));
+console.log(ht.get("surname"));
+console.log(ht.get("age"));
+console.log(ht.remove("surname"));
+console.log(ht.get("surname"));
+ht.remove("age");
+console.log(ht.get("age"));
